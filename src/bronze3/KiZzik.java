7cddb6a3d02e7b142e96ff7172f4cef1466b59f3
woozink <k0109974@naver.com>
2022-03-06 14:54:25 +0900

    2

---
+++ src/bronze3/KiZzik.java
@@ -0,0 +1,16 @@
+package bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class KiZzik {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = N; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}
--- src/bronze3/Star2.java
+++ src/bronze3/Star2.java
@@ -10,11 +10,11 @@ public class Star2 {
 
         int n = Integer.parseInt(br.readLine());
 
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n-i; j ++){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for(int k = 1; k <= i; k++){
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
             System.out.println();

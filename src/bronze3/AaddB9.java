0254c9f8d97ca3d7e42cd0cdc5763be21676d3aa
woozink <k0109974@naver.com>
2022-03-07 13:25:53 +0900

    3

---
+++ src/bronze3/AaddB9.java
@@ -0,0 +1,25 @@
+package bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class AaddB9 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+            System.out.println(a + b);
+
+        }
+
+    }
+}

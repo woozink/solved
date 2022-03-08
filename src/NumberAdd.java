ea793d76f1f557418ecf57c15be67f5567c666d9
woozink <k0109974@naver.com>
2022-03-08 14:40:32 +0900

    3

---
+++ src/NumberAdd.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NumberAdd {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//        int sum = 0;
+//        String b = br.readLine();
+//
+//        for(int i = 0; i < n; i++){
+//            sum += b.charAt(i)-48;
+//        }
+//        System.out.println(sum);
+
+
+//        int n = Integer.parseInt(br.readLine());
+//        String[] b = br.readLine().split("");
+//        int sum = 0;
+//
+//        for(int i =0; i<n; i++){
+//            sum += Integer.parseInt(b[i]);
+//        }
+//        System.out.println(sum);
+
+    }
+}

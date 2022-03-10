310042ba4f5565ff73310ebb7174325804a2e23a
woozink <k0109974@naver.com>
2022-03-10 16:25:20 +0900

    2

---
+++ src/bronze3/Rest17.java
@@ -0,0 +1,25 @@
+package bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Rest17 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] arr = new boolean[42];
+
+
+        for (int i = 0; i < 10; i++) {
+            arr[Integer.parseInt(br.readLine()) % 42] = true;
+        }
+        int count = 0;
+        for (boolean value : arr) {
+            if (value) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+}

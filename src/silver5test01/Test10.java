a047371644caa34160bd6ac9b28fb27a84be26eb
woozink <k0109974@naver.com>
2022-03-19 20:00:06 +0900

    2

--- src/silver5test01/Test10.java
+++
@@ -1,5 +0,0 @@
-package silver5test01;
-
-//https://www.acmicpc.net/problem/2609
-public class Test10 {
-}
--- src/silver5test01/Test9.java
+++
@@ -1,5 +0,0 @@
-package silver5test01;
-
-//https://www.acmicpc.net/problem/1158
-public class Test9 {
-}

7c370372b82662b41c3bb700f87b0c7611808833
woozink <k0109974@naver.com>
2022-03-20 15:53:35 +0900

    3

---
+++ src/silver5/GCD2.java
@@ -0,0 +1,36 @@
+package silver5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class GCD2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        for (int i = 0; i < T; i++) {
+            st =new StringTokenizer(br.readLine()," ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int x = gcd(a,b);
+
+            sb.append(a*b/x).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static int gcd(int a, int b){
+       while( b != 0){
+           int r = a %b;
+           a= b;
+           b =r;
+       }
+       return a;
+    }
+}

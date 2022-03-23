39a80f6ddfd41be4acc9961bc899b3b877125a1b
woozink <k0109974@naver.com>
2022-03-23 14:35:26 +0900

    3

---
+++ src/silver4/Bracket05.java
@@ -0,0 +1,41 @@
+package silver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Bracket05 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            sb.append(solve(br.readLine())).append('\n');
+        }
+    }
+
+    public static String solve(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.charAt(); i++) {
+            char c = s.charAt(i);
+            //여는 괄호일 경우 스택에 넣는다.
+
+            if (c == '(') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
+                return "NO";
+            } else {
+                stack.pop();
+            }
+        }
+        if (stack.empty()) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+}

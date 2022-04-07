0807852e98dc395b493763b9c411e09017b08869
woozink <k0109974@naver.com>
2022-04-07 12:18:29 +0900

    1

---
+++ src/silver3/Tile20.java
@@ -0,0 +1,35 @@
+package silver3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Tile20 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(Tile(N));
+
+
+    }
+
+    public static int Tile(int N) {
+        if (N == 1) {
+            return 1;
+        }
+        if (N == 2) {
+            return 2;
+        }
+
+        int val1 = 1;
+        int val2 = 2;
+        int sum = 0;
+        for(int i = 2; i < N; i++){
+            sum =(val2+val1) %15746;
+            val1 = val2;
+            val2 = sum;
+        }
+        return sum;
+    }
+}

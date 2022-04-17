f914e3955bbdb0755e509f79fdd9b2c5b09c02c2
woozink <k0109974@naver.com>
2022-04-17 12:13:32 +0900

    2

--- src/silver2Re/Meetingroom04.java
+++ src/silver2Re/Meetingroom04.java
@@ -1,4 +1,53 @@
 package silver2Re;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
 public class Meetingroom04 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        // 끝 시간과 시작시간 비교해 주어야하니까 정렬 시작시간 기준으로 하자
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        int count = 0;
+        int endTime = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i][0] >= endTime) {
+                endTime = arr[i][1];
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }

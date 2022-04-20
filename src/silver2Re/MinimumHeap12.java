0fe9afa3d10f2c61d75d491ee99d5d48bc9b2194
woozink <k0109974@naver.com>
2022-04-20 22:03:35 +0900

    3

---
+++ src/silver2Re/MinimumHeap12.java
@@ -0,0 +1,27 @@
+package silver2Re;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class MinimumHeap12 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+
+        int n = Integer.parseInt(br.readLine());
+        int val = 0;
+
+        for(int i = 0; i < n; i ++){
+            val = Integer.parseInt(br.readLine());
+            if(val != 0){
+                q.add(val);
+            }else if(q.isEmpty()){
+                System.out.println("0");
+            }else{
+                System.out.println(q.poll());
+            }
+        }
+    }
+}

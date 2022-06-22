d16c53ab3fbe3432b67f51e000a9a8c34e531fb7
woozink <k0109974@naver.com>
2022-06-22 22:15:50 +0900

    1

--- src/Npixel/One.java
+++ src/Npixel/One.java
@@ -1,12 +1,13 @@
 package Npixel;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class One {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
     }
 }
